0256425464544d538c55015ac81e946c10f7e0b8
Delacour America <damerica@mi-p2.21-school.ru>
2019-12-01 18:53:22 +0300

    Закончил флаги

--- ft_printf/printf.c
+++ ft_printf/printf.c
@@ -12,7 +12,34 @@
 
 #include "printf.h"
 
-void create(const char *format, int start, int end, va_list ap)
+char *ft_strjoin_one_elem(char *s1, char elem)
+{
+    char	*newstr;
+	int		i;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	newstr = (char *)malloc(sizeof(char) * (i + 2));
+	if (!newstr)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		newstr[i] = s1[i];
+		i++;
+	}
+    newstr[i] = elem;
+    newstr[i + 1] = '\0';
+	return (newstr);
+}
+
+/*ft_create_str(t_spec *elem, va_list ap)
+{
+    
+}*/
+
+void ft_create_elem(const char *format, int start, int end, va_list ap)
 {
     t_spec *elem;
     int i;
@@ -28,21 +55,24 @@ void create(const char *format, int start, int end, va_list ap)
     i = 0;
     while (start <= end)
     {
-        /*while (format[start] == '+' || format[start] == '-' || format[start] == ' ')
+        while (format[start] == '+' || format[start] == '-' || format[start] == ' ' || format[start] == '0')
         {
             if (ft_strchr(elem->flag, format[start]) == NULL)
             {
                 if (ft_strlen(elem->flag) == 0)
-                    elem->flag = ft_strjoin(elem->flag, format[start]);
+                    elem->flag = ft_strjoin_one_elem(elem->flag, format[start]);
                 else
-                {   
-                    free(elem->flag);
+                {
+                    str = elem->flag;
+                    size = ft_strlen(elem->flag);
+                    ft_strdel(&elem->flag);
+                    elem->flag = (char *)malloc(sizeof(char) * (size + 1));
+                    elem->flag = str;
+                    elem->flag = ft_strjoin_one_elem(elem->flag, format[start]);
                 }
-                
-                size++;
             }
             start++;
-        }*/
+        }
         // ^^^ Не закончил данную часть ^^^
         if (format[start] == '#')
         {
@@ -64,22 +94,34 @@ void create(const char *format, int start, int end, va_list ap)
                 start++;
             }
         }
-        /*if (format[start] == 'h' || format[start] == 'l' || format[start] == 'L')
+        if (format[start] == 'h' || format[start] == 'l' || format[start] == 'L')
         {
             elem->modif = (char *)malloc(sizeof(char) * 3);
             if (format[start + 1] == 'h' || format[start + 1] == 'l')
             {
-                
+                elem->modif[0] = format[start];
+                elem->modif[1] = format[start + 1];
+                start = start + 2;
             }
-            elem->modif = format[start];
-            start++;
-        }*/
+            else
+            {
+                elem->modif[0] = format[start];
+                start++;
+            }
+        }
         // ^^^ Не закончил данную часть ^^^
         elem->conv_type = (char)malloc(sizeof(char) * 2);
         elem->conv_type = format[end];
         start++;
     }
+    //printf("elem->conv-type %c\n", elem->conv_type);
+    //printf("elem->flag |%s|\n", elem->flag);
+    //printf("elem->precision %i\n", elem->precision);
+    //printf("ele->field %i\n", elem->field);
+    //printf("ele->modif %s\n", elem->modif);
+    ft_create_str(elem, ap);
 }
+
 
 int ft_printf(const char *format, ...)
 {
@@ -94,12 +136,13 @@ int ft_printf(const char *format, ...)
         if (format[i] == '%')
         {
             k = i;
-            while (format[i] != 'c' || format[i] != 's' || format[i] != 'p' 
-            || format[i] != 'd' || format[i] != 'i' || format[i] != 'o' || 
-            format[i] != 'u' || format[i] != 'x' || format[i] != 'X' || format[i] != 'f')
+            while (format[i] != 'c' && format[i] != 's' && format[i] != 'p' 
+            && format[i] != 'd' && format[i] != 'i' && format[i] != 'o' && 
+            format[i] != 'u' && format[i] != 'x' && format[i] != 'X' && format[i] != 'f')
             {
                 i++;
             }
+            printf("k = %c\n i = %c\n", format[k], format[i]);
             create(format, k + 1, i, ap);
         }
         else
@@ -117,7 +160,7 @@ int main()
 {
     int a;
 
-    a = 4;
-    ft_printf("Hello World %i", a);
+    a = 15;
+    ft_printf("Hello% +-+-10.2ld", a);
     return (0);
 }
